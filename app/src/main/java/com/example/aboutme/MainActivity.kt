cd450bd76975d0e01cc77576f1621b3001ca7e7e
JODonovanKelly <JODonovanKelly@gmail.com>
2022-07-07 05:35:55 +0100

    Added a new Kotlin classs for handling the nickname button
    Signed-off-by: JODonovanKelly <JODonovanKelly@gmail.com>

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -12,19 +12,23 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val myName: MyName = MyName("Jonathan O\'Donovan Kelly")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-    binding.doneButton.setOnClickListener{
-        addNickname(it)
-    }
+        binding.myName = myName
+
+        binding.doneButton.setOnClickListener{
+            addNickname(it)
+        }
 
     }
     private fun addNickname(view: View) {
 
         binding.apply {
-            nicknameText.text = binding.nicknameEdit.text
+            myName?.nickname = nicknameEdit.text.toString()
             invalidateAll()
             nicknameEdit.visibility = View.GONE
             doneButton.visibility = View.GONE
